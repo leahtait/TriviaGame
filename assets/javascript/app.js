6e74e70d7a8c211682ba412f801cdb405a3991ae
Leah Tait <leah.tait@gmail.com>
2019-04-24 14:22:08 -0700

    HTML shell and pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+// on page load, timer starts counting down from 120 seconds 
+
+// function to evaluate user guesses
+
+// function to calculate user scores
+
+// when time runs out or user clicks submit, user score is diplayed 
