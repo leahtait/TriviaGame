6707761456575281f0a67e97bc6482388f901827
Leah Tait <leah.tait@gmail.com>
2019-04-24 14:57:32 -0700

    adding question and answer array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,72 @@
 // on page load, timer starts counting down from 120 seconds 
 
+//variables
+
+//variables to hold array of quiz questions and answers
+
+var myQuestions = [
+	{
+		question: "How old was the oldest living chicken?",
+		answers: {
+			a: '5 years',
+			b: '2 years',
+            c: '12 years',
+            d: '22 years'
+		},
+		correctAnswer: 'c'
+    },
+    {
+		question: "When a hen stops laying eggs and tries to sit on them to hatch them, what is the name of that condition?",
+		answers: {
+			a: 'Going broody',
+			b: 'Egg sitty',
+            c: 'Going sitty',
+            d: 'Nesty'
+		},
+		correctAnswer: 'a'
+	},
+    {
+		question: "How big was the world's largest chicken?",
+		answers: {
+			a: '17 pounds',
+			b: '23 pounds',
+            c: '29 pounds',
+            d: '37 pounds'
+		},
+		correctAnswer: 'b'
+    },
+    {
+		question: "Chickens were first domesticated for what purpose?",
+		answers: {
+			a: 'Eggs',
+			b: 'Meat',
+            c: 'House pets',
+            d: 'Cockfighting'
+		},
+		correctAnswer: 'd'
+    },
+    {
+		question: "What is the total population of chickens living today?",
+		answers: {
+			a: '19 million',
+			b: '115 million',
+            c: '1.6 billion',
+            d: '19 billion'
+		},
+		correctAnswer: 'd'
+	},
+	{
+		question: "Mike the headless chicken became famous for surving how long after being beheaded?",
+		answers: {
+			a: '2 weeks ',
+			b: '6 months',
+            c: '18 months',
+            d: '2 years'
+		},
+		correctAnswer: 'c'
+	}
+];
+
 // function to evaluate user guesses
 
 // function to calculate user scores
