8bf9557716201b755e5e4ccdbf76039be7c76c60
Leah Tait <leah.tait@gmail.com>
2019-04-27 10:32:12 -0700

    working timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -67,8 +67,118 @@ var myQuestions = [
 	}
 ];
 
-// function to evaluate user guesses
+//variables to populate HTML 
 
-// function to calculate user scores
 
-// when time runs out or user clicks submit, user score is diplayed 
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (--timer < 0) {
+            //stop timer
+            display.text("Times up!"); 
+            //stop quiz
+         showResults(); //doesn't work
+
+        }
+    }, 1000);
+}
+
+jQuery(function ($) {
+    // var twoMinutes = 60 * 2, two minute timer
+    var twoMinutes = 4, //4 second test timer
+        display = $('#time');
+    startTimer(twoMinutes, display);
+});
+
+
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+    function showQuestions(questions, quizContainer){
+        //output= user answers, answer = correct answer key
+        var output = [];
+        var answers;
+//for loop to loop over each question
+        for(var i=0; i<questions.length; i++){
+            
+            // first reset the list of answers
+            answers = [];
+
+            //add a radio selector for each question in the HTML
+            for(letter in questions[i].answers){
+                answers.push(
+                    '<label>'
+                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                    + '</label>'
+                );
+            }
+
+            // also put in question and answers
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        //take radio selector, questions, answers, and put it all into the page in one string
+        quizContainer.innerHTML = output.join('');
+    }
+
+
+    function showResults(questions, quizContainer, resultsContainer){
+        
+        // put answers into variable
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        
+        // keep track of submitted answers and set intital correct to 0
+        var userAnswer = '';
+        var numCorrect = 0;
+        
+        // loop through each question
+        for(var i=0; i<questions.length; i++){
+
+            // get the user's input
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+            
+            if(userAnswer===questions[i].correctAnswer){
+                // if they get it right, increase the correct number
+                numCorrect++;
+
+            }
+            // if they get it wrong
+            else{
+                // display wrong answer?
+                answerContainers[i].style.color = 'red';
+            }
+        }
+
+        // show total correct out of toal possible
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+
+    // display questions
+    showQuestions(questions, quizContainer);
+    
+    //display results
+    submitButton.onclick = function(){
+        showResults(questions, quizContainer, resultsContainer);
+    }
+
+}
+
