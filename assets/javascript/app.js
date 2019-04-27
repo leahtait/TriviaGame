2151a2a531b429e69bf8c5db832fdcb51bb5235d
Leah Tait <leah.tait@gmail.com>
2019-04-27 16:15:02 -0700

    some improvements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,13 @@
-// on page load, timer starts counting down from 120 seconds 
 
 //variables
 
 //variables to hold array of quiz questions and answers
 
 var myQuestions = [
-	{
-		question: "How old was the oldest living chicken?",
+    {
+        question: "How old was the oldest living chicken?",
 		answers: {
-			a: '5 years',
+            a: '5 years',
 			b: '2 years',
             c: '12 years',
             d: '22 years'
@@ -16,9 +15,9 @@ var myQuestions = [
 		correctAnswer: 'c'
     },
     {
-		question: "When a hen stops laying eggs and tries to sit on them to hatch them, what is the name of that condition?",
+        question: "When a hen stops laying eggs and tries to sit on them to hatch them, what is the name of that condition?",
 		answers: {
-			a: 'Going broody',
+            a: 'Going broody',
 			b: 'Egg sitty',
             c: 'Going sitty',
             d: 'Nesty'
@@ -26,9 +25,9 @@ var myQuestions = [
 		correctAnswer: 'a'
 	},
     {
-		question: "How big was the world's largest chicken?",
+        question: "How big was the world's largest chicken?",
 		answers: {
-			a: '17 pounds',
+            a: '17 pounds',
 			b: '23 pounds',
             c: '29 pounds',
             d: '37 pounds'
@@ -36,9 +35,9 @@ var myQuestions = [
 		correctAnswer: 'b'
     },
     {
-		question: "Chickens were first domesticated for what purpose?",
+        question: "Chickens were first domesticated for what purpose?",
 		answers: {
-			a: 'Eggs',
+            a: 'Eggs',
 			b: 'Meat',
             c: 'House pets',
             d: 'Cockfighting'
@@ -46,9 +45,9 @@ var myQuestions = [
 		correctAnswer: 'd'
     },
     {
-		question: "What is the total population of chickens living today?",
+        question: "What is the total population of chickens living today?",
 		answers: {
-			a: '19 million',
+            a: '19 million',
 			b: '115 million',
             c: '1.6 billion',
             d: '19 billion'
@@ -56,9 +55,9 @@ var myQuestions = [
 		correctAnswer: 'd'
 	},
 	{
-		question: "Mike the headless chicken became famous for surving how long after being beheaded?",
+        question: "Mike the headless chicken became famous for surving how long after being beheaded?",
 		answers: {
-			a: '2 weeks ',
+            a: '2 weeks ',
 			b: '6 months',
             c: '18 months',
             d: '2 years'
@@ -67,38 +66,41 @@ var myQuestions = [
 	}
 ];
 
-//variables to populate HTML 
 
+// on page load, timer starts counting down from 120 seconds 
 
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
-
+        
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
-
+        
         display.text(minutes + ":" + seconds);
-
+        
         if (--timer < 0) {
             //stop timer
             display.text("Times up!"); 
             //stop quiz
-         showResults(); //doesn't work
-
+            showResults(); //I can't get this to work. I think it's a scoping problem but I don't know how to fix it.  
+            
         }
     }, 1000);
+    
 }
 
+//setting the 2 minute timer
 jQuery(function ($) {
-    // var twoMinutes = 60 * 2, two minute timer
-    var twoMinutes = 4, //4 second test timer
-        display = $('#time');
+    var twoMinutes = 60 * 2;
+    display = $('#time');
     startTimer(twoMinutes, display);
+    
 });
 
 
+//variables to populate HTML 
 
 var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
@@ -119,12 +121,12 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
             answers = [];
 
             //add a radio selector for each question in the HTML
-            for(letter in questions[i].answers){
+            for(answerLetter in questions[i].answers){
                 answers.push(
                     '<label>'
-                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-                        + letter + ': '
-                        + questions[i].answers[letter]
+                        + '<input type="radio" name="question'+i+'" value="'+answerLetter+'">'
+                        + answerLetter + ': '
+                        + questions[i].answers[answerLetter]
                     + '</label>'
                 );
             }
@@ -159,17 +161,18 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
             if(userAnswer===questions[i].correctAnswer){
                 // if they get it right, increase the correct number
                 numCorrect++;
-
+                 //  display this next to question answered correctly
+                answerContainers[i].append(" -- You got this one right!");
             }
             // if they get it wrong
             else{
-                // display wrong answer?
-                answerContainers[i].style.color = 'red';
+                //  display this next to question answered incorrectly
+                answerContainers[i].append(" -- You got this one wrong!");
             }
         }
 
-        // show total correct out of toal possible
-        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+        // show total correct out of total possible
+        resultsContainer.innerHTML = 'Your final score is ' + numCorrect + ' out of ' + questions.length + '. Well done!';
     }
 
     // display questions
